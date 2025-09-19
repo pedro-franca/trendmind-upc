52e2d832286ea3747c2507c600d8c528b0a545c2
Pedro <pedrochfr@gmail.com>
2025-09-19 15:21:07 +0200

    Add to ticker_filtered jsons and export them to mongodb

--- 01_data_ingestion.py
+++ 01_data_ingestion.py
@@ -1,12 +1,8 @@
-from delta import configure_spark_with_delta_pip
-import pandas as pd
-import numpy as np
-import json
 import yfinance as yf
 import os
 from deltalake import DeltaTable
 from deltalake.writer import write_deltalake
-from pyspark.sql import SparkSession
+
 
 
 def load_financial_data(ticker):
@@ -21,7 +17,7 @@ def load_financial_data(ticker):
     """
     DELTA_OUTPUT_PATH = f"./data/{ticker}_yfinance"
     # Fetch data from Yahoo Finance
-    df = yf.download(ticker, start="2020-01-01", end="2025-08-25")
+    df = yf.download(ticker, start="2020-01-01", end="2023-08-25")
     df = df.droplevel('Ticker', axis=1)
     df['ticker'] = ticker
     if df.empty:
@@ -32,61 +28,6 @@ def load_financial_data(ticker):
 
     print(f"✅ Exported {len(df)} rows to {DELTA_OUTPUT_PATH}")
 
-def load_ticker_json(ticker):
-    """
-    Load ticker data from a JSON file.
-    
-    Args:
-        ticker (str): The stock ticker symbol.
-    Returns:
-        pd.DataFrame: DataFrame containing ticker data.
-    """
-    json_path = "./data/merged_deduped_news_v1.json"
-
-    if not os.path.exists(json_path):
-        raise ValueError(f"JSON file not found at path: {json_path}")
-
-    data = json.load(open(json_path, "r", encoding="utf-8"))
-    print(f"Total feed items in JSON: {len(data['feed'])}")
-    ticker_feeds = []
-    for item in data['feed']:
-        if "ticker_sentiment" in item:
-            for ticker_info in item["ticker_sentiment"]:
-                if ticker_info.get("ticker") == ticker:
-                    ticker_feeds.append(item)
-                    break # Add break to avoid adding the same item multiple times if 'ORCL' appears multiple times in ticker_sentiment
-    print(f"Total feed items for {ticker}: {len(ticker_feeds)}")
-
-        # Save filtered JSON temporarily
-    temp_json_path = f"./data/{ticker}_filtered.json"
-    with open(temp_json_path, "w", encoding="utf-8") as f:
-        json.dump(ticker_feeds, f, ensure_ascii=False, indent=2)
-
-    local_jars = "./jars/delta-spark_2.13-4.0.0.jar,./jars/delta-storage-4.0.0.jar,./jars/antlr4-runtime-4.13.1.jar"
-
-
-    spark = SparkSession.builder \
-    .appName("JSON to Delta") \
-    .config("spark.jars", local_jars) \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .getOrCreate()
-
-    # Read JSON into Spark DataFrame
-    df = spark.read.option("multiline", "true").json(temp_json_path)
-
-    # Inspect schema (optional, useful for debugging)
-    df.printSchema()
-    df.show(5)
-
-    # Define Delta output path
-    delta_path = f"./data/{ticker}_news"
-
-    # Write DataFrame as Delta
-    df.repartition(10).write.format("delta").mode("overwrite").save(delta_path)
-
-    print(f"JSON data written to Delta at {delta_path}")
-
 
 
 if __name__ == "__main__":
@@ -94,12 +35,9 @@ if __name__ == "__main__":
     ticker = "ORCL"
     try:
         load_financial_data(ticker)
-        load_ticker_json(ticker)
         yf_data = DeltaTable(f"./data/{ticker}_yfinance")
-        news_data = DeltaTable(f"./data/{ticker}_news")
         print(f"Data loaded for {ticker}:")
         print(yf_data.to_pandas().head())
-        print(news_data.to_pandas().head())
 
     except ValueError as e:
         print(e)
--- 02_data_cleaning.py
+++ 02_data_cleaning.py
@@ -1,41 +1,17 @@
-from delta import configure_spark_with_delta_pip
+import json
 import duckdb
 import pandas as pd
 from deltalake import DeltaTable
-from pyspark.sql.functions import explode, col
-from pyspark.sql import SparkSession
+from pymongo import MongoClient
 
-# --- Step 1: Read from Delta Lake ---
+
+# --- Read yf from Delta Lake ---
 def read_yf_df(ticker):
     delta_path = f"./data/{ticker}_yfinance"
     dt = DeltaTable(delta_path)
     df = dt.to_pandas()
     return df
 
-def read_news_df(ticker):
-    delta_path = f"./data/{ticker}_news"
-    dt = DeltaTable(delta_path)
-    df = dt.to_pandas()
-    # Explode the 'ticker_sentiment' column (which is a list of dicts) into separate rows
-    exploded_df = df.explode('ticker_sentiment').reset_index(drop=True)
-
-    # Now each row has a single ticker_sentiment dict; normalize it into separate columns
-    ticker_df = pd.json_normalize(exploded_df['ticker_sentiment'])
-
-    # Concatenate with the original DataFrame
-    exploded_df = pd.concat([exploded_df.drop(columns=['ticker_sentiment']), ticker_df], axis=1)
-
-    # Filter for ticker 'ORCL' and select relevant columns
-    sentiment_df = exploded_df[exploded_df['ticker'] == ticker][[
-        'relevance_score',
-        'ticker_sentiment_score',
-        'time_published'
-    ]].rename(columns={
-        'relevance_score': 'orcl_relevance_score',
-        'ticker_sentiment_score': 'orcl_sentiment_score'
-    })
-    print(sentiment_df.head())  # Optional preview
-    return sentiment_df
 
 #### If we want to perform any cleaning, we can do it here.
 # For example, let's drop rows with any NaN values.
@@ -43,32 +19,51 @@ def clean_df(df):
     df_cleaned = df.dropna()
     return df_cleaned
 
-# --- Step 2: Write to DuckDB ---
+# --- Write to DuckDB ---
 def export_to_duckdb(ticker):
     yf_df = read_yf_df(ticker)
     yf_df = clean_df(yf_df)
-    news_df = read_news_df(ticker)
-    news_df = clean_df(news_df)
+
     print(yf_df.head()) # Optional preview
-    print(news_df.head())  # Optional preview
 
     output_yf_path = f"./data/{ticker}_yf_cleaned.duckdb"
-    output_news_path = f"./data/{ticker}_news_cleaned.duckdb"
+
     # Export yf_df to DuckDB
     con = duckdb.connect(database=output_yf_path, read_only=False)
     con.execute("CREATE OR REPLACE TABLE stock_data AS SELECT * FROM yf_df")
     con.close()
     print(f"✅ Data exported to {output_yf_path}")
-    # Export news_df to DuckDB
-    con = duckdb.connect(database=output_news_path, read_only=False)
-    con.execute("CREATE OR REPLACE TABLE news_data AS SELECT * FROM news_df")
-    con.close()
-    print(f"✅ Data exported to {output_news_path}")
+
+# --- Write news to MongoDB ---
+def export_news_to_mongodb(ticker):
+
+    json_path = f"./data/news/{ticker}_filtered.json"
+
+    with open(json_path, 'r') as f:
+        json_data = json.load(f)
+    
+    MONGO_DB = "financial_news"
+    MONGO_COLLECTION = f"{ticker}_news"
+
+    if isinstance(json_data, dict):
+        json_data = [json_data]
+    
+    # Connect to MongoDB (make sure MongoDB is running and accessible)
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client[MONGO_DB]
+    collection = db[MONGO_COLLECTION]
+
+    collection.insert_many(json_data)
+    print(f"✅ Inserted {len(json_data)} news docs into MongoDB {MONGO_DB}.{MONGO_COLLECTION}")
+
+
 
 if __name__ == "__main__":
     ticker = "ORCL"
     try:
         export_to_duckdb(ticker)
         print(f"✅ Data exported for {ticker}.")
+        export_news_to_mongodb(ticker)
+        print(f"✅ News exported for {ticker}.")
     except Exception as e:
         print(f"❌ Error processing {ticker}: {e}")
