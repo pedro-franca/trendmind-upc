8dc5172ca45e6c5ef633865bcf09e7f9a3999e1f
Pedro <pedrochfr@gmail.com>
2025-10-31 13:11:59 +0100

    format to pct

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ if ticker:
     df = df[cols]
 
     # ---------- PLOT PRICE HISTORY ----------
-    fig = px.line(df, x=df.index[-50:], y=df["returns"].values[-50:], title=f"{ticker} Returns")
-    fig.update_layout(xaxis_title="Date", yaxis_title="Price Returns")
+    fig = px.line(df, x=df.index[-50:], y=df["returns"].values[-50:]*100, title=f"{ticker} Returns")
+    fig.update_layout(xaxis_title="Date", yaxis_title="Price Returns (%)")
 
     # ---------- LOAD MODEL ----------
     model = keras.models.load_model(f"artifacts/best_model_{ticker}_4.keras", custom_objects={'combined_loss': combined_loss})
@@ -58,7 +58,7 @@ if ticker:
         scaler = joblib.load(f'artifacts/best_scaler_{ticker}_4.joblib')
         scaled_data = scaler.fit_transform(df)
         forecast_df = forecast_future(model, df, scaler, feature_columns=cols, n_future=1, n_past=days_past[ticker])
-        predicted_price = forecast_df["Predicted_returns"]
+        predicted_price = forecast_df["Predicted_returns"]*100
 
         # ---------- DISPLAY PREDICTION ----------
         # Generate future dates (same as before)
